ef2ce6d181562a9a0448797e43e10e017834e5d3
Luiz Fernando <96695552+Luizfernando022@users.noreply.github.com>
2022-05-04 18:07:28 -0300

    Att1

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
+let TaskCount = 0
+
+onload = () =>{
+
+  main.innerHTML = localStorage.getItem('tasks')
+  if(main != ""){
+    let imgs = document.querySelectorAll('img')
+
+    imgs.forEach(element => { 
+      element.addEventListener('click',clearTask)
+      let toggle = document.querySelectorAll(".toggle");
+    toggle.forEach((element) => {
+      element.addEventListener("click", toggleAnimation);
+    });
+  });
+  }
+
+}
+
 function toggleAnimation() {
   if (this.style.backgroundColor != "lime") {
-    let icon = document.getElementsByClassName("icon")[0];
-    this.style.backgroundColor = "lime";
-    this.children[0].style.animationName = "toggle";
-    vez = 1;
+    this.style.backgroundColor = 'lime'
+    this.children[0].style.animationName = "toggleOn";
   } else {
-    this.style.backgroundColor = "red";
-    this.children[0].style.animationName = "teste";
-    vez = 0;
+    this.style.backgroundColor = 'red'
+    this.children[0].style.animationName = "toggleOff";
   }
 }
 
 let btn = document.getElementsByTagName("button")[0];
 
-btn.addEventListener("click", CreateTask);
+btn.addEventListener("click", createTask);
 
-function CreateTask() {
+
+
+function createTask() {
+
+  howManyTasks()
+
   let input = document.getElementsByTagName("input")[0];
   let main = document.getElementById("main");
   if (input.value == "") {
@@ -27,15 +48,30 @@ function CreateTask() {
     let content = createElement("div");
     content.classList.add("content");
     console.log(content);
-    createContentOfContent(content, input);
-    main.appendChild(content);
+    createContentOfContent(content, input)
+    main.appendChild(content)
 
     let toggle = document.querySelectorAll(".toggle");
 
     toggle.forEach((element) => {
       element.addEventListener("click", toggleAnimation);
     });
+    
+ let imgs = document.querySelectorAll('img')
+
+  imgs.forEach(element => { 
+    element.addEventListener('click',clearTask)
+});
+
+  localStorage.setItem('tasks',main.innerHTML)
+  input.value = ""
+  input.focus()
   }
+}
+let tasks = document.getElementById('task')
+function howManyTasks(){
+  TaskCount++
+  tasks.innerHTML = "Tasks: " + TaskCount
 }
 
 function createContentOfContent(content, input) {
@@ -47,7 +83,7 @@ function createPDiv(content, input) {
   let divP = document.createElement('div')
   divP.classList.add('divP')
   let p = createElement("p");
-  p.innerHTML = String(input.value);
+  p.innerHTML = String(input.value).toLowerCase()
   divP.appendChild(p);
   content.appendChild(divP)
 }
@@ -73,4 +109,12 @@ function createToggle(content) {
   icon.classList.add("icon");
   toggle.appendChild(icon);
   content.appendChild(toggle);
+}
+
+function clearTask(){
+  TaskCount--
+  tasks.innerHTML = "Tasks: " + TaskCount
+  let elemento = this.parentElement.parentElement
+  elemento.parentElement.removeChild(elemento)
+  localStorage.setItem('tasks',main.innerHTML)
 }
